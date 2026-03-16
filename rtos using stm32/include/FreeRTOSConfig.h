8577653e91782b7c618acaa812cfa291677760d5
perlin-cyber-god <perlin25101@iiitnr.edu.in>
2026-03-17 02:12:16 +0530

    config: configure FreeRTOS kernel and interrupt priorities for Cortex-M3

--- rtos using stm32/include/FreeRTOSConfig.h
+++ rtos using stm32/include/FreeRTOSConfig.h
@@ -14,12 +14,16 @@
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
 
+// Interrupt priority settings for ARM Cortex-M3
+#define configKERNEL_INTERRUPT_PRIORITY         255
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY    191 // Priority 6 (0xC0 >> 4)
+
 // Set the following definitions to 1 to include the API function, or zero
 // to exclude the API function.
 #define INCLUDE_vTaskPrioritySet                1
-#define INCLUDE_uxTaskPriorityGet                1
+#define INCLUDE_uxTaskPriorityGet               1
 #define INCLUDE_vTaskDelete                     1
-#define INCLUDE_vTaskCleanUpResources            0
+#define INCLUDE_vTaskCleanUpResources           0
 #define INCLUDE_vTaskSuspend                    1
 #define INCLUDE_vTaskDelayUntil                 1
 #define INCLUDE_vTaskDelay                      1
@@ -29,4 +33,4 @@
 #define xPortPendSVHandler  PendSV_Handler
 #define xPortSysTickHandler SysTick_Handler
 
-#endif
+#endif /* FREERTOS_CONFIG_H */
